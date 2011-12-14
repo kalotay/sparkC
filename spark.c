9e83c74b01f95c1e37a16b1e8a281465e7084b96
Dan Kalotay <d.kalotay@gmail.com>
2011-12-14 20:31:46 +0100

    modularised code
    Bug: won't correctly parse space seperated input

--- spark.c
+++ spark.c
@@ -7,7 +7,13 @@
 /* limit input numbers */
 #define MAX_INPUT 1024
 
-int main(int argc, char* argv[])
+int parse_input(char *input,
+                double values[],
+                double *min_value,
+                double *max_value,
+                size_t n);
+
+int main(int argc, char *argv[])
 {
     int n_elements = 0;
     double elements[MAX_INPUT];
@@ -15,43 +21,21 @@ int main(int argc, char* argv[])
     double max_element = -HUGE_VAL;
 
     if (argc < 2)
+        /* TODO: print help message */
         return -1;
 
     int i;
     for (i = 1; i < argc; ++i) {
-        char* process_from = argv[i];
-        char* to_be_processed = argv[i];
-
-        while (*process_from != '\0') {
-            double value = strtod(process_from, &to_be_processed);
-            int err_strtod = errno;
-            /* strtod did not recognise a number */
-            if (process_from == to_be_processed) {
-                ++process_from;
-                ++to_be_processed;
-            /* strtod did recognise a number */
-            } else {
-                /* avoid overflow and underflow */
-                if (err_strtod != ERANGE) {
-                    elements[n_elements++] = value;
-                    min_element = value < min_element ? value : min_element;
-                    max_element = value > max_element ? value : max_element;
-
-                    /* array full; get out of here */
-                    if (n_elements == MAX_INPUT)
-                        goto cleanup;
-                }
-
-                /* to_be_processed was shifted to point to the character
-                 * following the last on of the number. Shift process_from to
-                 * point to the same character */
-                process_from = to_be_processed;
-            }
+        n_elements = parse_input(argv[i],
+                                 elements,
+                                 &min_element,
+                                 &max_element,
+                                 MAX_INPUT- n_elements);
+        if (n_elements >= MAX_INPUT) {
+            break;
         }
     }
 
-cleanup:
-    /* assert used to shut up compiler. labels cannot be at declerations */
     assert(min_element <= max_element);
     double scaling_factor = max_element - min_element;
     /* Avoid division by zero if all elements have the same value */
@@ -66,3 +50,43 @@ cleanup:
     return 0;
 }
 
+int parse_input(char *input,
+                double values[],
+                double *min_value,
+                double *max_value,
+                size_t n)
+{
+    char* process_from = input;
+    char* to_be_processed = input;
+    int n_elements = 0;
+
+    while (*process_from != '\0') {
+        /* array full; get out of here */
+        if (n_elements == n)
+            break;
+
+        double value = strtod(process_from, &to_be_processed);
+        int err_strtod = errno;
+        /* strtod did not recognise a number */
+        if (process_from == to_be_processed) {
+            ++process_from;
+            ++to_be_processed;
+        /* strtod did recognise a number */
+        } else {
+            /* avoid overflow and underflow */
+            if (err_strtod != ERANGE) {
+                values[n_elements++] = value;
+                *min_value = value < *min_value ? value : *min_value;
+                *max_value = value > *max_value ? value : *max_value;
+
+            }
+
+            /* to_be_processed was shifted to point to the character
+             * following the last on of the number. Shift process_from to
+             * point to the same character */
+            process_from = to_be_processed;
+        }
+    }
+
+    return n_elements;
+}
